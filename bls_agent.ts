86bb9b667502d1220bd6afd9d45d5da10a17683a
Geoff Flarity <geoff@centml.ai>
2024-12-28 18:32:44 -0500

    update cron properly

--- bls_agent.ts
+++ bls_agent.ts
@@ -3,8 +3,13 @@ import { executeWorkflow } from "./lib/workflow.ts";
 // TODO account for Daylight Saving Time later
 // 8:31 ET in UTC 31 13 * * *
 // 10:01 ET in UTC 1 15 * * *
-Deno.cron("checkBLS", "* * * * *", async () => {
+Deno.cron("checkBLS 13 31", "31 13 * * *", async () => {
     // TODO skip weekends
     executeWorkflow(true, 5);
 }); 
 
+
+Deno.cron("checkBLS 15 01", "1 15 * * *", async () => {
+    // TODO skip weekends
+    executeWorkflow(true, 5);
+}); 

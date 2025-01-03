25d52babb1f6e3e889577be2f0bbcc878815becc
Geoff Flarity <geoff@centml.ai>
2025-01-03 14:23:30 -0500

    apply caught up

--- bls_agent.ts
+++ bls_agent.ts
@@ -13,8 +13,3 @@ Deno.cron("checkBLS 15 01", "1 15 * * *", async () => {
     // TODO skip weekends
     executeWorkflow(true, 5);
 }); 
-
-Deno.cron("every five minutes for catch up", "*/5 * * * *", async () => {
-    // in the last 6 hours for catch up
-    executeWorkflow(true, 6*60);
-}); 

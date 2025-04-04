83ad157eb736f5283f8cc3edebd295036ea307db
Geoff Flarity <geoff@centml.ai>
2025-04-04 09:57:19 -0400

    fix timezones

--- bls_agent.ts
+++ bls_agent.ts
@@ -1,9 +1,9 @@
 import { executeWorkflow } from "./lib/workflow.ts"
 
-// Some BLS posting happen at 9:30 ET, so we should check at 9:31 ET
+// Some BLS posting happen at 8:30 ET, so we should check at 8:31 ET
 // Double up as an easy way to account for dailight savings time
-// 8:31 ET in UTC 31 14 * * * or 31 13 * * *
-Deno.cron("checkBLS 14 31", "31 14 * * *", async () => {
+// 8:31 ET in UTC 31 12 * * * or 31 13 * * *
+Deno.cron("checkBLS 14 31", "31 12 * * *", async () => {
     // TODO skip weekends
     executeWorkflow(true, 5)
 })
@@ -13,12 +13,12 @@ Deno.cron("checkBLS 13 31", "31 13 * * *", async () => {
 })
 
 // Some BLS posting happen at 10:00 ET, so we should check at 10:01 ET
-// 10:01 ET in UTC 1 15 * * * or 1 14 * * *
-Deno.cron("checkBLS 15 01", "1 15 * * *", async () => {
+// 10:01 ET in UTC 1 14 * * * or 1 15 * * *
+Deno.cron("checkBLS 14 01", "1 14 * * *", async () => {
     // TODO skip weekends
     executeWorkflow(true, 5)
 })
-Deno.cron("checkBLS 14 01", "1 14 * * *", async () => {
+Deno.cron("checkBLS 15 01", "1 15 * * *", async () => {
     // TODO skip weekends
     executeWorkflow(true, 5)
 })
